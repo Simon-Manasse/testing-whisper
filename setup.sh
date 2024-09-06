fc911739356a98d7c517f8ee41b350aa5abb1a82
Simon-Manasse <simo0047@hz.nl>
2024-09-06 15:04:01 +0200

    added vosk model to setup file

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Creating virtual environment for python to negate problems
 python -m venv .venv
+# Activating virtual environment
 source .venv/bin/activate
+# Installing needed packages
 pip install -r requirements.txt
+
+# Downloading vosk model
+wget https://alphacephei.com/vosk/models/vosk-model-small-en-us-0.15.zip
+unzip vosk-model-small-en-us-0.15.zip
+mv vosk-model-small-en-us-0.15 model
